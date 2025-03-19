91409eed14081c3fd8c8ebcab55e20213ec30bd8
Chandi977 <pappumahato000@gmail.com>
2025-03-19 08:08:44 +0530

    nothing have done

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
 // Export the database name to be used in the application
 export const DB_NAME = "zenith"; // Name of the MongoDB database
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// export const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
