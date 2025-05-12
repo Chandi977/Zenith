af5846fb4b68baf9db23b5ad7e8419e88ffcc91e
Chandi977 <pappumahato000@gmail.com>
2025-05-12 15:16:00 +0530

    try

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ app.use(
     credentials: true, // Enable sending credentials (e.g., cookies) with CORS requests
   })
 );
+app.use(express.json());
 
 // Middleware to parse JSON request bodies with a limit of 16kb
 app.use(express.json({ limit: "16kb" }));
